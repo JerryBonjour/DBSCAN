f59caea4df5b6f882fa5e4bc7005f2b14315efb5
lorenwel <lorenwel@student.ethz.ch>
2016-12-15 16:23:31 +0100

    Optimizations.

--- include/dbscan/dbscan.h
+++ include/dbscan/dbscan.h
@@ -6,6 +6,38 @@
 
 #include "nanoflann/nanoflann.hpp"
 
+/// \brief Custom result set class that only returns number of neighbors and their indices.
+///        Default version also returns distances which we don't need.
+template <typename DistanceType, typename IndexType = size_t>
+class RadiusCountResultSet {
+public:
+  const DistanceType radius_;
+
+  std::vector<IndexType>& m_indices_;
+
+  inline RadiusCountResultSet(DistanceType radius, std::vector<IndexType>& m_indices) :
+      radius_(radius), m_indices_(m_indices) {
+    init();
+  }
+
+  inline ~RadiusCountResultSet() {}
+
+  inline void init() { clear(); }
+  inline void clear() { m_indices_.clear(); }
+
+  inline size_t size() const { return m_indices_.size(); }
+
+  inline bool full() const { return true; }
+
+  inline void addPoint(DistanceType dist, IndexType index) {
+    if (dist < radius_) m_indices_.push_back(index);
+  }
+
+  inline DistanceType worstDist() const { return radius_; }
+
+};
+
+/// \brief Base class for DBSCAN algorithm
 template <typename Derived, class Distance = nanoflann::metric_L2_Simple>
 class DbscanBase {
 
@@ -20,43 +52,40 @@ class DbscanBase {
 
     size_t count;
     std::list<IndType> new_neighbors;
+
+    inline void clear() {
+      count = 0u;
+      new_neighbors.clear();
+    }
   };
 
-  void loadData();
-
   void expandCluster(std::list<IndType>& neighbors, const int& cur_cluster) {
-    QueryReturn query;
     for (auto&& cur_neighbor : neighbors) {
       if (!queried_[cur_neighbor]) {
         queried_[cur_neighbor] = true;
 
-        query = regionQuery(cur_neighbor);
-        if (query.count >= min_pts_) {
-          setCluster(query.new_neighbors, cur_cluster);
-          neighbors.splice(neighbors.end(), query.new_neighbors);
+        regionQuery(cur_neighbor);
+        if (query_.count >= min_pts_) {
+          setCluster(query_.new_neighbors, cur_cluster);
+          neighbors.splice(neighbors.end(), query_.new_neighbors);
         }
       }
     }
   }
 
-  QueryReturn regionQuery(const IndType& query_index) {
-    std::vector<std::pair<IndType, Scalar> > ret_matches;
-
-    QueryReturn output;
+  inline void regionQuery(const IndType& query_index) {
+    result_set_.clear();
+    query_.clear();
     // Extract query point to nanoflann format.
-    std::vector<Scalar> query_pt(dim_);
-    for (IndType i = 0; i < dim_; ++i) query_pt[i] = points_(query_index, i);
+    for (IndType i = 0; i < dim_; ++i) query_pt_[i] = points_(query_index, i);
     // Perform radius search.
-    const size_t n_matches =
-        kd_tree_.index->radiusSearch(&query_pt[0], eps_, ret_matches, params_);
-    for (size_t i = 0u; i < n_matches; ++i) {
-      const IndType cur_idx = ret_matches[i].first;
+    kd_tree_.index->radiusSearchCustomCallback(&query_pt_[0], result_set_, params_);
+    for (auto&& cur_idx: ret_matches_) {
       if (cur_idx != query_index) {	// Self point is no match.
-        ++output.count;
-        if ((*clusters_)(cur_idx) == 0) output.new_neighbors.push_back(cur_idx);
+        ++query_.count;
+        if ((*clusters_)(cur_idx) == 0) query_.new_neighbors.push_back(cur_idx);
       }
     }
-    return output;
   }
 
   void setCluster(const std::list<IndType>& indices, const int& cur_cluster) {
@@ -78,6 +107,14 @@ class DbscanBase {
   const size_t min_pts_;
   nanoflann::SearchParams params_;
 
+  // Variables to avoid reallocation during exectution.
+  std::vector<Scalar> query_pt_;
+  std::vector<IndType> ret_matches_;
+  RadiusCountResultSet<Scalar, IndType> result_set_;
+  QueryReturn query_;
+  std::list<IndType> expansion_queue_;
+
+  // KD Tree.
   KDTreeType kd_tree_;
 
 public:
@@ -86,7 +123,8 @@ public:
   /// \param eps Maximum distance for a point to be considered part of a cluster.
   /// \param min_pts Minimum number of points in vincinity to be considered a base point.
   DbscanBase(MatRefType points, const Scalar& eps, const unsigned int& min_pts) :
-      points_(points), n_pts_(points.rows()), dim_(points.cols()), eps_(eps*eps), min_pts_(min_pts),
+      points_(points), n_pts_(points.rows()), dim_(points.cols()), eps_(eps*eps),
+      min_pts_(min_pts), query_pt_(dim_), result_set_(eps_, ret_matches_),
       kd_tree_(dim_, points_, 20 /* max leafs */ ) {
     params_.sorted = false;
   }
@@ -104,12 +142,13 @@ public:
       if(!queried_[i]) {
         queried_[i] = true;
 
-        QueryReturn query = regionQuery(i);
+        regionQuery(i);
 
-        if(query.count >= min_pts_) {
+        if(query_.count >= min_pts_) {
+          expansion_queue_.swap(query_.new_neighbors);
            (*clusters_)(i) = cur_cluster;
-          setCluster(query.new_neighbors, cur_cluster);
-          expandCluster(query.new_neighbors, cur_cluster);
+          setCluster(expansion_queue_, cur_cluster);
+          expandCluster(expansion_queue_, cur_cluster);
 
           ++cur_cluster;
           CHECK_NE(cur_cluster, std::numeric_limits<int>::max())
